62ef840ead29b0439b28bf989790d8464c51a818
Alinga <websupport@alinga.com.au>
2024-12-24 19:47:46 +0530

    Revert login url changes

--- app/controllers/disco_app/concerns/user_authenticated_controller.rb
+++ app/controllers/disco_app/concerns/user_authenticated_controller.rb
@@ -13,7 +13,7 @@ module DiscoApp::Concerns::UserAuthenticatedController
        Rails.logger.info "::::::: UserAuthenticatedController shopify_user ::::::: #{session[:shopify_user]&.inspect} :::::::"
       @user = DiscoApp::User.find(session[:shopify_user])
     rescue ActiveRecord::RecordNotFound
-      redirect_to "#{main_app.root_path}auth/shopify_user?shop=#{sanitized_shop_name}"
+      redirect_to disco_app.new_user_session_path
     end
 
 end
